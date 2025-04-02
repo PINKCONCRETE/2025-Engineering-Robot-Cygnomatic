b4b1995172c99df33c8f8e3d9c94f707f0690858
PINKCONCRETE <155738325@qq.com>
2025-04-02 10:46:39 +0800

    commit message

---
+++ EC/controller/controller/Core/Src/main.c
@@ -0,0 +1,255 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "adc.h"
+#include "dma.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+typedef union{
+	int16_t data;
+	uint8_t bytes[4];
+} int16_t_u;
+typedef struct {
+    float x;  // ״̬����ֵ
+    float P;  // ״̬Э�������ֵ
+} KalmanFilter;
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+extern DMA_HandleTypeDef hdma_adc1;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+/* �ض���fputc���� */
+int fputc(int ch, FILE *f) {
+	HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+	return ch;
+}
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
+{
+	HAL_ADC_Stop_DMA(&hadc1);
+}
+
+// �������˲���ʼ������
+void kalman_init(KalmanFilter *kf, float initial_value) {
+    kf->x = initial_value;
+    kf->P = 1.0;  // ��ʼЭ�������ֵ�����Ը���ʵ���������
+}
+
+// �������˲����º���
+float kalman_update(KalmanFilter *kf, float adc_value, float A, float H, float Q, float R) {
+    // Ԥ�ⲽ��
+    float x_pred = A * kf->x;
+    float P_pred = A * kf->P * A + Q;
+    // ���²���
+    float K = P_pred * H / (H * P_pred * H + R);
+    kf->x = x_pred + K * (adc_value - H * x_pred);
+    kf->P = (1 - K * H) * P_pred;
+    return kf->x;
+}
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_ADC1_Init();
+  MX_USART1_UART_Init();
+  MX_USART3_UART_Init();
+  /* USER CODE BEGIN 2 */
+	HAL_ADCEx_Calibration_Start(&hadc1);
+	uint16_t ADC_Values[6];
+	KalmanFilter ADC_Kalman_Filter[6];
+	HAL_ADC_Start_DMA(&hadc1,(uint32_t*)&ADC_Values, 6);
+	HAL_Delay(100);
+	uint8_t tx_data[1+2*6];//1+2*6
+	tx_data[0] = 0x0A;
+	int16_t temp;
+	int i;
+	for(i = 0;i < 6;i++)
+	{
+		kalman_init(&ADC_Kalman_Filter[i], ADC_Values[i]);
+	}
+	//printf("Init end\n");
+	
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+		HAL_ADC_Start_DMA(&hadc1,(uint32_t*)&ADC_Values, 6);
+		HAL_Delay(50);
+		for(i = 0;i < 6;i++)
+		{
+			kalman_update(&ADC_Kalman_Filter[i], ADC_Values[i],1, 1, 0.01, 0.1);
+		}
+		printf("%d,%d,%d,%d,%d,%d\n", (int)ADC_Kalman_Filter[0].x, (int)ADC_Kalman_Filter[1].x, (int)ADC_Kalman_Filter[2].x, (int)ADC_Kalman_Filter[3].x,(int)ADC_Kalman_Filter[4].x, (int)ADC_Kalman_Filter[5].x);
+		for(i = 0;i < 6;i++)
+		{
+			temp = ADC_Kalman_Filter[i].x;
+			memcpy(&tx_data[1 + i * sizeof(short)], &temp, sizeof(short));
+		}
+		HAL_UART_Transmit_DMA(&huart3, tx_data, sizeof(tx_data));
+    /* USER CODE END WHILE */
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC;
+  PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV2;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ EC/controller/controller/MDK-ARM/controller/startup_stm32f103xe.d
@@ -0,0 +1,1 @@
+controller\startup_stm32f103xe.o: startup_stm32f103xe.s
